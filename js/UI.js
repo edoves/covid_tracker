5a789b2a116fe6b6261d9762114ee8b67b20a703
dove <ndoverte11@gmail.com>
2022-10-15 20:47:01 +0800

    done coding the search country feature covid tracker

--- js/UI.js
+++ js/UI.js
@@ -1,10 +1,10 @@
 class UI {
   #formatNumber(num) {
-    return String(num).replace(/(.)(?=(\d{3})+$)/g, '$1,');
+    return String(num).replace(/(.)(?=(\d{3})+$)/g, '$1,')
   }
 
   getWorldData(covidStat) {
-    const worldStatusContainer = document.querySelector('.world__status');
+    const worldStatusContainer = document.querySelector('.world__status')
     //get only the word data
 
     worldStatusContainer.innerHTML = ` <div class="status__item">
@@ -18,11 +18,11 @@ class UI {
   <div class="status__item">
     <h2>${this.#formatNumber(covidStat.recovered.value)}</h2>
     <p class="recovered">recovered</p>
-  </div>`;
+  </div>`
   }
 
   getPhilData(covidStat) {
-    const philStatusContainer = document.querySelector('.phil__status');
+    const philStatusContainer = document.querySelector('.phil__status')
 
     philStatusContainer.innerHTML = ` <div class="status__item">
         <h2>${this.#formatNumber(covidStat.confirmed.value)}</h2>
@@ -35,10 +35,38 @@ class UI {
       <div class="status__item">
         <h2>${this.#formatNumber(covidStat.recovered.value)}</h2>
         <p class="recovered">recovered</p>
-      </div>`;
+      </div>`
+  }
+
+  displayMessage(msg, clsName, currentElmt) {
+    const divEl = document.createElement('div')
+    divEl.className = clsName
+    divEl.textContent = msg
+    console.log(divEl)
+    currentElmt.appendChild(divEl)
+
+    setTimeout(() => {
+      divEl.remove()
+    }, 1500)
+  }
+
+  displayCoutryStat(covidCountryStats, country) {
+    const { confirmed, deaths, recovered } = covidCountryStats
+
+    const searchResult = document.querySelector('.search__result')
+
+    searchResult.innerHTML = `
+      <div class="flag">
+            <img src="https://countryflagsapi.com/svg/${country}" alt="" />
+      </div>
+      <ul>
+        <li> Confirmed: ${this.#formatNumber(confirmed.value)}  </li>
+        <li>  Deaths: ${this.#formatNumber(deaths.value)}  </li>
+        <li> Recovered: ${this.#formatNumber(recovered.value)} </li>
+      </ul>`
   }
 }
 
-const ui = new UI();
+const ui = new UI()
 
-export default ui;
+export default ui
